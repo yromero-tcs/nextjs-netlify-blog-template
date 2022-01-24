8c2f686926149147a53c36a120a93a0f1f78bd94
Yael Romero <yromero@truechoicesolutions.com>
2022-01-24 14:17:59 -0500

    feat: update index paths

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,45 @@
 import Link from 'next/link';
 
-const Index = () => (
-  <div>
-    Hello World.{' '} <br/>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-    <br/>
-    <Link href="/part1">
-      <a>Part 1</a>
-    </Link>
-  </div>
-);
+const glob = require('glob');
+
+const Index = (props) => {
+  const { paths, names } = props;
+  
+  return ( 
+    <div>
+      Hello World.{' '} <br/>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <br/>
+      {paths.map((path, index) => {
+        return <div key={index}>
+        <Link href={path}>
+          <a>{names[index]}</a>
+        </Link>
+        <br/>
+        </div>
+      })}
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const attributeFiles = glob.sync('content/attributes/**/*.en.md');
+
+  const attributes = attributeFiles.map(file =>
+    file
+      .split('/')[2]
+      .split('.')[0]
+      .trim()
+  )
+
+  return {
+    props: {
+      paths: attributes.map(attributeName => `/part1/${attributeName}`),
+      names: attributes
+    }
+  }
+}
 
 export default Index;
